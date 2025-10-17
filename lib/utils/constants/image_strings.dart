3f5d1e88804ee4628400b5cfe428bda2f7cce1c3
Sehal Hasan <sehal.hasan16@gmail.com>
2025-10-17 10:06:53 +0530

    updated logo paths

--- lib/utils/constants/image_strings.dart
+++ lib/utils/constants/image_strings.dart
@@ -2,10 +2,10 @@
 class TImages {
 
   // -- App Logos
-  static const String darkAppLogo = "assets/logos/t-store-splash-logo-black.png";
-  static const String lightAppLogo = "assets/logos/t-store-splash-logo-white.png";
+  static const String darkAppLogo = "assets/logos/marketscope-logo.png";
+  static const String lightAppLogo = "assets/logos/marketscope-logo.png";
 
   // -- Social Logos
   static const String google = "assets/logos/google-icon.png";
-  static const String facebook = "assets/logos/facebook-icon.png";
+  // static const String facebook = "assets/logos/facebook-icon.png";
 }
