c7c8080abb15d05fb5f0e043b46b6d558ca1e894
Sehal Hasan <sehal.hasan16@gmail.com>
2025-11-12 09:52:33 +0530

    Long time commits

--- lib/app.dart
+++ lib/app.dart
@@ -1,23 +1,21 @@
+//  
 import 'package:flutter/material.dart';
 import 'package:ibb_store/utils/theme/theme.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
 
-
   @override
   Widget build(BuildContext context) {
-
     return MaterialApp(
-
-      themeMode: ThemeMode.system,//flutter now detects the theme of the system and applies accoridnglu
-      //when we do not provide any them, flutter default theme is uesed
-      //wehen we do provide, it is treated under the Light mode
-
+      themeMode: ThemeMode.system,
       darkTheme: TAppTheme.darkTheme,
-      theme: TAppTheme.lightTheme //theme is for lighttheme
+      theme: TAppTheme.lightTheme,
+      home: const Scaffold( // Add a temporary home screen
+        body: Center(
+          child: Text('Home Screen'),
+        ),
+      ),
     );
   }
-  
-
 }
